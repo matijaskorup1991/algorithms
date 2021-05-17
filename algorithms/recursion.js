7f78c17c87938c3fc745396cc8b529e3159e936a
matijaskorup1991 <smatija1991@gmail.com>
2021-05-17 14:01:45 +0200

    recursion2

--- algorithms/recursion.js
+++ algorithms/recursion.js
@@ -8,4 +8,13 @@ function countDown(num) {
   countDown(num - 1);
 }
 
-countDown(100);
+// countDown(100);
+
+//SUM RANGE
+
+function sumRange(num) {
+  if (num === 1) return 1;
+  return num + sumRange(num - 1);
+}
+
+console.log(sumRange(10));
