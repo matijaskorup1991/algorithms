c05599c04a35ec7eac349e80903b3578692c4748
matijaskorup1991 <smatija1991@gmail.com>
2021-05-17 15:20:40 +0200

    recursion6

--- algorithms/recursion.js
+++ algorithms/recursion.js
@@ -50,3 +50,10 @@ function power(a, b) {
 }
 
 // console.log(power(2, 3));
+
+function productOfArray(arr) {
+  if (arr.length === 0) return 1;
+  return arr[0] * productOfArray(arr.slice(1));
+}
+
+console.log(productOfArray([1, 2, 3, 4]));
