fcb11f851e54676da6e736e0c24eb81b6d88a163
matijaskorup1991 <smatija1991@gmail.com>
2021-05-17 16:28:59 +0200

    recursion8

--- algorithms/recursion2.js
+++ algorithms/recursion2.js
@@ -4,3 +4,13 @@ function reverse(str) {
 }
 
 // console.log(reverse('matija'));
+
+const isOdd = (v) => v % 2 === 0;
+
+function someRecursive(arr, fn) {
+  if (arr.length === 0) return false;
+  if (arr[0]) return true;
+  return someRecursive(arr.slice(1), fn);
+}
+
+// console.log(someRecursive([1, 2, 3, 4], isOdd));
