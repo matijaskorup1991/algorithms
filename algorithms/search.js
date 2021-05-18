1cab648963e95d3cf70fb3f092e0bbd27862b9cf
matijaskorup1991 <smatija1991@gmail.com>
2021-05-18 13:51:25 +0200

    search2

--- algorithms/search.js
+++ algorithms/search.js
@@ -5,3 +5,42 @@ function linearSearch(arr, el) {
 }
 
 console.log(linearSearch([1, 2, 3, 4, 5, 6, 7], 5));
+
+//BINARY SEARCH:
+
+function binarySearch(arr, el) {
+  let start = 0;
+  let end = arr.length - 1;
+  let mid = Math.floor((start + end) / 2);
+  while (arr[mid] !== el && start <= end) {
+    if (el < arr[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+    mid = Math.floor((start + end) / 2);
+  }
+  if (arr[mid] === el) {
+    return mid;
+  }
+  return -1;
+}
+
+function binarySearch(arr, el) {
+  let start = 0;
+  let end = arr.length - 1;
+  let mid = Math.floor((start + end) / 2);
+
+  while (arr[mid] !== el && start <= end) {
+    if (el < arr[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+    mid = Math.floor((start + end) / 2);
+  }
+  if (arr[mid] === el) {
+    return mid;
+  }
+  return -1;
+}
