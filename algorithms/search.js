25d07d2c4412503c5dcf408ad87b3c64fdd47ee0
matijaskorup1991 <smatija1991@gmail.com>
2021-05-18 13:52:15 +0200

    search3

--- algorithms/search.js
+++ algorithms/search.js
@@ -25,22 +25,3 @@ function binarySearch(arr, el) {
   }
   return -1;
 }
-
-function binarySearch(arr, el) {
-  let start = 0;
-  let end = arr.length - 1;
-  let mid = Math.floor((start + end) / 2);
-
-  while (arr[mid] !== el && start <= end) {
-    if (el < arr[mid]) {
-      end = mid - 1;
-    } else {
-      start = mid + 1;
-    }
-    mid = Math.floor((start + end) / 2);
-  }
-  if (arr[mid] === el) {
-    return mid;
-  }
-  return -1;
-}
