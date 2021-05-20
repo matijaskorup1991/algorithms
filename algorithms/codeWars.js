2aae01e5324ab6cc0464a206afcabc05b1fee41f
matijaskorup1991 <smatija1991@gmail.com>
2021-05-20 10:56:49 +0200

    codeWars2

--- algorithms/codeWars.js
+++ algorithms/codeWars.js
@@ -8,4 +8,35 @@ function toUnderscore(str) {
   return res.substring(1);
 }
 
-console.log(toUnderscore('TestController'));
+// Write a function called sumIntervals/sum_intervals() that accepts an array of intervals,
+// and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
+
+// Intervals
+// Intervals are represented by a pair of integers in the form of an array.
+// The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+
+// Overlapping Intervals
+// List containing overlapping intervals:
+
+function sumIntervals(arr) {
+  let res = [];
+
+  arr.forEach((el) => {
+    for (let i = el[0]; i < el[el.length - 1]; i++) {
+      res.push(i);
+    }
+  });
+
+  let set = new Set(res);
+  return Array.from(set).length;
+}
+
+console.log(
+  sumIntervals([
+    [1, 5],
+    [10, 20],
+    [1, 6],
+    [16, 19],
+    [5, 11],
+  ])
+);
