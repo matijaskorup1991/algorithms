6ab180f1d73e55d66e6a7978db48e93129145a0c
matijaskorup1991 <smatija1991@gmail.com>
2021-06-05 08:01:40 +0200

    test2

--- algorithms/eloquent.js
+++ algorithms/eloquent.js
@@ -63,11 +63,55 @@ for (let i = 20; ; i++) {
 //   return result;
 // };
 
-function power(base, exponent) {
-  if (exponent === 0) {
-    return 1;
+// function power(base, exponent) {
+//   if (exponent === 0) {
+//     return 1;
+//   }
+//   return base * power(base, exponent - 1);
+// }
+
+// console.log(power(2, 2));
+
+// function findSolution(target) {
+//   function find(current, history) {
+//     if (current === target) {
+//       return history;
+//     } else if (current > target) {
+//       return null;
+//     } else {
+//       return (
+//         find(current + 5, `(${history} + 5)`) ||
+//         find(current * 3, `${history} * 3`)
+//       );
+//     }
+//   }
+//   return find(1, '1');
+// }
+
+// console.log(findSolution(24));
+
+// function farmInventory(cows, chickens) {
+//   let cowString = String(cows);
+//   while (cowString.length < 3) {
+//     cowString = '0' + cowString;
+//   }
+//   console.log(`${cowString} Cows`);
+
+//   let chickensString = String(chickens);
+//   while (chickensString.length < 3) {
+//     chickensString = '0' + chickensString;
+//   }
+//   console.log(`${chickensString} Chickens`);
+// }
+
+// farmInventory(3, 68);
+
+function countLetters(str, letter) {
+  let counter = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === letter) counter++;
   }
-  return base * power(base, exponent - 1);
+  return counter;
 }
 
-console.log(power(2, 2));
+console.log(countLetters('testBBB', 't'));
