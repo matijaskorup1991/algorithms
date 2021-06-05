95d653002a9f9fdc41c33d68b88afbc9a9a84b25
matijaskorup1991 <smatija1991@gmail.com>
2021-06-05 06:27:47 +0200

    test

--- algorithms/eloquent.js
+++ algorithms/eloquent.js
@@ -11,10 +11,46 @@ for (let i = 20; ; i++) {
   }
 }
 
-let res = '';
-let counter = 0;
-while (counter < 7) {
-  res = res + '#';
-  counter++;
-  console.log(res);
-}
+// let res = '';
+// let counter = 0;
+// while (counter < 7) {
+//   res = res + '#';
+//   counter++;
+//   console.log(res);
+// }
+
+// function chessboard(size) {
+//   let res = '';
+
+//   for (let i = 0; i < size; i++) {
+//     for (let j = 0; j < size; j++) {
+//       if ((i + j) % 2 === 0) {
+//         res += ' ';
+//       } else {
+//         res += '#';
+//       }
+//     }
+//     res += '\n';
+//   }
+//   return res;
+// }
+
+// console.log(chessboard(8));
+
+// const humus = function (factor) {
+//   const ingredient = function (amount, unit, name) {
+//     let ingredientAmount = amount * factor;
+//     if (ingredientAmount > 1) {
+//       unit += 's';
+//     }
+//     console.log(`${ingredientAmount} ${unit} ${name}`);
+//   };
+//   ingredient(1, 'can', 'chickpeas');
+//   ingredient(0.25, 'cup', 'tahini');
+//   ingredient(0.25, 'cup', 'lemon juice');
+//   ingredient(1, 'clove', 'garlic');
+//   ingredient(2, 'tablespoon', 'olive oil');
+//   ingredient(0.5, 'teaspoon', 'cumin');
+// };
+
+// humus(3);
---
+++ algorithms/test2.js
@@ -0,0 +1,35 @@
+// document.querySelector('#test').addEventListener('click', (e) => {
+//   console.log(e.target.id);
+// });
+
+// let arr = [2, 4, 6];
+
+// let newArr = arr.map((item) => (item > 4 ? item * 2 : item));
+
+// console.log(arr);
+// console.log(newArr);
+
+// let obj = {
+//   name: 'Matija',
+// };
+
+// let spans = Array.from(document.querySelectorAll('span'));
+// console.log(spans);
+
+// let $ = (el) => document.querySelector(el);
+
+// let test = document.querySelector('#test');
+// let test = $('#test');
+
+let arr = ['Olesja', 'Esther', 'Matija', 'Severin'];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+// matrix(4x4)
+
+// ####
+// ####
+// ####
+// ####
