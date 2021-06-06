477644b5fcf0892d098ee01440aa1e7636c02c6c
matijaskorup1991 <smatija1991@gmail.com>
2021-06-06 08:35:18 +0200

    test1

--- algorithms/eloquent.js
+++ algorithms/eloquent.js
@@ -234,4 +234,14 @@ function repeat(n, fn) {
   }
 }
 
-console.log(repeat(3, console.log));
+// console.log(repeat(3, console.log));
+
+function unless(test, then) {
+  if (!test) then();
+}
+
+repeat(3, (n) => {
+  unless(n % 2 == 1, () => {
+    console.log(n, ' is even');
+  });
+});
