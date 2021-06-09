2bf2f604cab699464298291d83f66c004b11158e
matijaskorup1991 <smatija1991@gmail.com>
2021-06-09 14:22:30 +0200

    test

--- algorithms/test2.js
+++ algorithms/test2.js
@@ -1,35 +1,12 @@
-// document.querySelector('#test').addEventListener('click', (e) => {
-//   console.log(e.target.id);
-// });
-
-// let arr = [2, 4, 6];
-
-// let newArr = arr.map((item) => (item > 4 ? item * 2 : item));
-
-// console.log(arr);
-// console.log(newArr);
-
-// let obj = {
-//   name: 'Matija',
-// };
-
-// let spans = Array.from(document.querySelectorAll('span'));
-// console.log(spans);
-
-// let $ = (el) => document.querySelector(el);
-
-// let test = document.querySelector('#test');
-// let test = $('#test');
-
-let arr = ['Olesja', 'Esther', 'Matija', 'Severin'];
-
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+function getInxToIns(arr, num) {
+  let sorted = arr.sort((a, b) => a - b);
+  let counter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (sorted[i] < num) {
+      counter += 1;
+    }
+  }
+  return counter;
 }
 
-// matrix(4x4)
-
-// ####
-// ####
-// ####
-// ####
+console.log(getInxToIns([20, 3, 5], 19));
