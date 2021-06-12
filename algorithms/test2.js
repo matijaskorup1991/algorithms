5902f7e4bad9e17858bbcaa8b94b213ce4f06e3a
matijaskorup1991 <smatija1991@gmail.com>
2021-06-12 10:33:02 +0200

    circle

--- algorithms/test2.js
+++ algorithms/test2.js
@@ -249,4 +249,42 @@ function firstUnique(str) {
   return char ? char : -1;
 }
 
-console.log(firstUnique('loveleetcode'));
+// console.log(firstUnique('loveleetcode'));
+
+/**
+ * Judge Route Circle
+ *
+ * Initially, there is a Robot at position (0, 0). Given a sequence of its moves,
+ * judge if this robot makes a circle, which means it moves back to the original place.
+ *
+ * The move sequence is represented by a string. And each move is represent by a character.
+ * The valid robot moves are R (Right), L (Left), U (Up) and D (down).
+ *
+ * The output should be true or false representing whether the robot makes a circle.
+ *
+ * Example 1:
+ * Input: "UD"
+ * Output: true
+ *
+ * Example 2:
+ * Input: "LL"
+ * Output: false
+ */
+
+function routeCircle(input) {
+  let res = {
+    R: 0,
+    L: 0,
+    U: 0,
+    D: 0,
+  };
+
+  for (let item of input) {
+    res[item]++;
+  }
+
+  if (res.R === res.L && res.U === res.D) return true;
+  return false;
+}
+
+// console.log(routeCircle('LL'));
